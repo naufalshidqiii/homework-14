a64c8182a9ceef72cb637db59a317bc69f1331a1
Naufal Shidqi <naufalshidqi99@gmail.com>
2023-04-10 19:02:18 +0700

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,32 +4,31 @@ import Books from "../components/Books";
 import Cookies from "js-cookie";
 
 export default function Homepage(props) {
-  return (
-    <Wrapper>
-      {props?.books?.map((book) => (
-        <Books key={`${book.id} ${book.title}`} {...book} />
-      ))}
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			{props?.books?.map((book) => (
+				<Books key={`${book.id} ${book.title}`} {...book} />
+			))}
+		</Wrapper>
+	);
 }
 
 // server side props ( fetch in server )
 
 export async function getServerSideProps() {
-  try {
-    const books = await prisma.book.findMany({
-      orderBy: {
-        title: "asc",
-      },
-    });
-    return {
-      props: {
-        books,
-      },
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(400).json({ message: "Something went wrong" });
-  }
-  
+	try {
+		const books = await prisma.book.findMany({
+			orderBy: {
+				title: "asc",
+			},
+		});
+		return {
+			props: {
+				books,
+			},
+		};
+	} catch (err) {
+		console.log(err);
+		return res.status(400).json({ message: "Something went wrong" });
+	}
 }
